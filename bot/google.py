985b29b94b3d244b5c2f430b7f407900f5acd9a6
Cory Levy <corylevy@gmail.com>
2018-02-25 19:10:06 +0200

    Occaisonal commit

--- bot/google.py
+++ bot/google.py
@@ -6,6 +6,7 @@
 # =================================================================================================================== #
 # ===================================================== IMPORTS ====================================================== #
 
+import os
 import httplib2
 
 from apiclient import discovery
@@ -17,7 +18,13 @@ from oauth2client.file import Storage
 
 CREDENTIALS_FILE_PATH = ""
 CONFESSION_SHEET_ID = "1eyPP0nEnivMe9fS_y1Z8EKwY02f8rETxKK1RmaRlKYs"
-CONFESSIONS_FORM_RANGE_A1_FORMAT = "Form Responses 1!B:C"
+READY_CONFESSIONS_A1_FORMAT = "Form Responses 1!B:C"
+SCOPES = 'https://www.googleapis.com/auth/spreadsheets'
+APPLICATION_NAME = 'Confession Manager'
+
+SHEETS_API_URL = "https://sheets.googleapis.com/$discovery/rest?version=v4"
+ROWS_DIMENSION = "ROWS"
+ARCHIVE_RANGE = "Archive!A:C"
 
 RAW_INPUT_OPTION = "RAW"
 PARSE_DATA_INPUT_OPTION = "USER_ENTERED"
@@ -27,32 +34,66 @@ PARSE_DATA_INPUT_OPTION = "USER_ENTERED"
 
 class Sheet(object):
     """
+    Represent a sheet, provides basic read/write operations.
     """
     def __init__(self, sheet_id):
         """
+        @param sheet_id: The id of the sheet, found in the google sheet web view.
+        @type sheet_id: str
         """
         # Load credentials file
-        store = Storage(CREDENTIALS_FILE_PATH)
+        store = self._get_credentials()
         self.credentials = store.get()
         self.id = sheet_id
 
         # Create the connection to Google Sheets
         self.connection = self.credentials.authorize(httplib2.Http())
-        discovery_url = ('https://sheets.googleapis.com/$discovery/rest?version=v4')
+        discovery_url = (SHEETS_API_URL)
         self.service = discovery.build('sheets', 'v4', http=self.connection, discoveryServiceUrl=discovery_url)
+
+    def _get_credentials():
+        """Gets valid user credentials from storage.
+
+        If nothing has been stored, or if the stored credentials are invalid,
+        the OAuth2 flow is completed to obtain the new credentials.
+
+        Returns:
+            Credentials, the obtained credential.
+        """
+        home_dir = os.path.expanduser('~')
+        credential_dir = os.path.join(home_dir, '.credentials')
+        if not os.path.exists(credential_dir):
+            os.makedirs(credential_dir)
+        credential_path = os.path.join(credential_dir, "google_sheets.json")
+
+        store = Storage(credential_path)
+        credentials = store.get()
+        if not credentials or credentials.invalid:
+            flow = client.flow_from_clientsecrets(CREDENTIALS_FILE_PATH, SCOPES)
+            flow.user_agent = APPLICATION_NAME
+            # TODO: Check if it breaks the code
+            credentials = tools.run(flow, store)
+
+        return credentials
 
     def get_data(self, data_range):
         """
+        Get data from the sheet.
+        @param data_range: The cells of the spreadsheet to retrieve. Specified in A1 format.
+        @type data_range: str
+        @return: The retrieved data
+        @rtype: list
         """
         # Get data from sheet
         result = self.service.spreadsheets().values().get(spreadsheetId=self.id, range=data_range).execute()
         return result.get("values", [])
 
-    def add_row(self, data, data_range, input_option_raw=True):
+    def add_row(self, data, data_range, raw_input_option=True):
         """
         """
-        input_option = RAW_INPUT_OPTION if input_option_raw else PARSE_DATA_INPUT_OPTION
+        input_option = RAW_INPUT_OPTION if raw_input_option else PARSE_DATA_INPUT_OPTION
 
+        # Construct the request parameters
         body = {
             "values": data
         }
@@ -62,6 +103,20 @@ class Sheet(object):
                                                     valueInputOption=input_option,
                                                     body=body).execute()
 
+    def delete_row(self, row_number):
+        """
+        """
+        body = {
+            "deleteDimension": {
+                "range": {
+                    "sheetId": self.id,
+                    "dimension": ROWS_DIMENSION,
+                    "startIndex": row_number - 1,
+                    "endIndex": row_number - 1
+                }
+            }
+        }
+
 
 class ConfessionsSheet(Sheet):
     """
@@ -69,7 +124,9 @@ class ConfessionsSheet(Sheet):
     def get_ready_confessions(self):
         """
         """
-        raw_confessions = self.get_data(CONFESSIONS_FORM_RANGE_A1_FORMAT)
+        raw_confessions = self.get_data(READY_CONFESSIONS_A1_FORMAT)
+
+        # Check if the confession is marked for publishing
         confessions = [confession for confession in raw_confessions if len(confession) > 1]
         return confessions
 
@@ -77,4 +134,4 @@ class ConfessionsSheet(Sheet):
         """
         """
         row = [date_received, date_published, confession]
-        self.add_row(list(row))
+        self.add_row(list(row), ARCHIVE_RANGE)
