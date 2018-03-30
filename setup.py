dcdcf44022972fc11b3f847cbc6390ec25886957
LevyCory <corylevy@icloud.com>
2018-03-30 16:48:27 +0300

    Made Confession Manager a Python package

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Confession Manager",
+    version="1.1",
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    url="https://github.com/LevyCory/confession_manager",
+    author="Cory Levy",
+    install_requires=[
+        "facebook-sdk",
+        "httplib2",
+        "requests",
+        "google-api-python-client"
+    ],
+    entry_points={}
+)
